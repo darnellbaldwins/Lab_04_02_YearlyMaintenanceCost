f05572b0b06f5e1c851dddbf60fa3c3a669cafb6
Darnell Baldwin <darnellbaldwin@Darnells-MacBook-Air.local>
2024-09-30 20:07:26 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+     int WinterMaintenance = 1250;
+     int SpringMaintenance = 760;
+     int SummerMaintenance = 950;
+     int FallMaintenance = 1000;
+     int fullMaintenanceCost = WinterMaintenance + SpringMaintenance ;
+        System.out.println("The yearly cost for maintenance is" + " " + fullMaintenanceCost );
+    }
+}
